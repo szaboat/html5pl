a266cfd9f2b803c833f938f2c3e2417a882cabe7
Attila Szabo <attila.szabo@gmail.com>
2010-04-14 22:15:50 +0200

    pause / play controls

--- plugin.js
+++ plugin.js
@@ -1,4 +1,9 @@
 $.fn.html5pl = function() {
+  
+  $().createControls();
+  
+  var paused;
+  
   $('a').each(function(){
     if($(this).attr('href').match(/mp3/)){
       audio = $('<audio>');
@@ -14,7 +19,44 @@ $.fn.html5pl = function() {
       $(this).parent().css('background',"yellow");
     }
   });
+  
+  $('.pause').live('click',function(){
+    var paused;
+    $('audio').each(function(){
+      if(!this.ended) {
+        this.pause();
+        paused = this;
+      }
+    });
+    $(this).html('play').addClass('play').removeClass('pause');
+  });
+  
+  $('.play').live('click',function(){
+    $('audio').each(function(){
+      if(this.paused) {
+        this.play();
+      }
+    });    
+    $(this).addClass('pause').html('pause').removeClass('play');
+  });
+  
 };
+
+$.fn.createControls = function(){
+  holder = $('<div>').css({
+    'position':'absolute',
+    'top':'0',
+    'right':'0',
+    'width': '35px',
+    'height': '20px',
+    'background':'yellow',
+    'color':'black',
+    'padding':'1em'
+  });
+  pause = $("<span class='pause'>pause</span>");
+  holder.append(pause);
+  $('body').append(holder);
+}
 
 $.fn.c = function() {
   console.log('foo');
